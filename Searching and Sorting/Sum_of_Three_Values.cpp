146c0704cf9503c5b044ce06ca6e0adef0d733aa
Schr0Sm1ley <radiantremel444@gmail.com>
2024-09-14 17:47:18 +0600

    Modified solution of one problem

--- Searching and Sorting/Sum_of_Three_Values.cpp
+++ Searching and Sorting/Sum_of_Three_Values.cpp
@@ -1,47 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define endl "\n"
 #define int long long int
-#ifdef KARIM
-#include "DEBUG_TEMPLATE.h"
-#else
-#define HERE
-#define debug(args...)
-#endif
-typedef pair<int,int> pii;
+#define trace(x) cout << #x << " : " << x << endl;
 const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const int N = 2e5 +5;
+const int INF = 1e18, MINF = -1e18;
+const int N = 2e5 + 5;
 
-void solution(){
-    int n,x;cin >> n >> x;
-    pair<int,int> p[n];
-    for(int i=0;i<n;i++){
+bool cmp(pair<int, int> a, pair<int, int> b) {
+    return a.first < b.first;
+} 
+void solution() {
+    int n, x;cin >> n >> x;
+    pair<int, int> p[n];
+    for (int i = 0; i < n; i++) {
         cin >> p[i].first;
-        p[i].second = i+1;
+        p[i].second = i + 1;
     }
-    sort(p,p+n);
-    for(int i=0,r;i<n;i++){
+    sort(p, p + n,cmp);
+    for (int i = 0; i < n - 2; i++) {
         int comp = x - p[i].first;
-        r = n - 1;
-        for(int l = i + 1; l < r; l++){
-            while(l + 1 < r && (p[l].first + p[r].first) > comp) r--;
-            if(p[l].first + p[r].first == comp){
-                cout << p[i].second << " " << p[l].second << " " << p[r].second ;
+        int l = i + 1, r = n - 1;
+        while(l < r) {
+            if(p[l].first + p[r].first == comp) {
+                cout << p[i].second << " " << p[l].second << " " << p[r].second << endl;
                 return;
+            }
+            else if(p[l].first + p[r].first > comp) {
+                r--;
+            }
+            else {
+                l++;
             }
         }
     }
     cout << "IMPOSSIBLE";
 }
 
-int32_t main(){
-    #ifndef KARIM
+int32_t main() {
     fastio;
-    #endif
     int t = 1;
-    while(t--){
+    while(t--) {
         solution();
     }
     return 0;
